f5e3d05705776e2b6efb491c4537a23f70dd57ef
MGianetti <mateusgianetti1996@gmail.com>
2022-08-07 22:00:29 -0300

    feat(NavBar): Implemented navbar component

---
+++ src/components/navbar/navbar.component.jsx
@@ -0,0 +1,51 @@
+import React, { memo, useState } from 'react';
+import { Box, Flex, Icon } from '@chakra-ui/react';
+
+import { FiMenu } from 'react-icons/fi';
+
+import BreadCrumb from './bread-crumb';
+import SearchInput from './search-input';
+import Configuration from './configuration';
+import ProfileAvatar from './profile-avatar';
+import Messages from './messages';
+import Notifications from './notifications';
+
+const Navbar = props => {
+  const { onHamburguerClick, isMenuExpanded } = props;
+
+  const sideMenuStyles = isMenuExpanded ? { width: 256 } : { width: 64 };
+
+  return (
+    <Box
+      w={`calc(100% - ${sideMenuStyles.width});`}
+      h="64px"
+      pos="sticky"
+      style={{
+        marginLeft: `${sideMenuStyles.width}px`,
+        transition: 'margin-left 0.5s',
+      }}
+    >
+      <Flex h="100%" justifyContent="space-between">
+        <Flex alignItems="center">
+          <Icon
+            as={FiMenu}
+            mr="32px"
+            onClick={onHamburguerClick}
+            cursor="pointer"
+          />
+          <BreadCrumb />
+        </Flex>
+
+        <Flex alignItems="center">
+          <SearchInput />
+          <Configuration />
+          <Notifications />
+          <Messages />
+          <ProfileAvatar />
+        </Flex>
+      </Flex>
+    </Box>
+  );
+};
+
+export default memo(Navbar);
