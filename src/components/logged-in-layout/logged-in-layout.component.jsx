948ef9dbef805d3edaf8e9fed183b2f8c083179e
MGianetti <mateusgianetti1996@gmail.com>
2022-08-07 22:01:58 -0300

    feat(LoggedInLayout): Implemented logged-in-layout component

---
+++ src/components/logged-in-layout/logged-in-layout.component.jsx
@@ -0,0 +1,33 @@
+import React, { memo, useState } from 'react';
+import { Flex } from '@chakra-ui/react';
+
+import Navbar from '../navbar';
+import ContentContainer from '../content-container';
+import SideMenu from '../side-menu';
+
+const LoggedInLayout = () => {
+  const [isMenuExpanded, setIsMenuExpanded] = useState(false);
+
+  const handleMenuExpansion = () => setIsMenuExpanded(true);
+  const handleMenuRetraction = () => setIsMenuExpanded(false);
+  const handleToggleMenu = () => setIsMenuExpanded(!isMenuExpanded);
+
+  return (
+    <>
+      <Navbar
+        onHamburguerClick={handleToggleMenu}
+        isMenuExpanded={isMenuExpanded}
+      />
+      <Flex height="calc(100vh - 64px);" minH="1024px">
+        <SideMenu
+          isMenuExpanded={isMenuExpanded}
+          onMouseEnter={handleMenuExpansion}
+          onMouseLeave={handleMenuRetraction}
+        />
+        <ContentContainer />
+      </Flex>
+    </>
+  );
+};
+
+export default memo(LoggedInLayout);
