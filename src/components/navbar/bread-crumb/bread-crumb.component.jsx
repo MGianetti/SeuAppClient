d8ba02dc0d548770620a985a5e6a90fbddf98db8
MGianetti <mateusgianetti1996@gmail.com>
2022-08-07 22:01:36 -0300

    feat(Breadcrumb): Implemented breadcrumb component

---
+++ src/components/navbar/bread-crumb/bread-crumb.component.jsx
@@ -0,0 +1,22 @@
+import React, { memo } from 'react';
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from '@chakra-ui/react';
+
+const BreadCrumb = () => {
+  return (
+    <Breadcrumb>
+      <BreadcrumbItem>
+        <BreadcrumbLink href="#">Home</BreadcrumbLink>
+      </BreadcrumbItem>
+
+      <BreadcrumbItem>
+        <BreadcrumbLink href="#">Docs</BreadcrumbLink>
+      </BreadcrumbItem>
+
+      <BreadcrumbItem isCurrentPage>
+        <BreadcrumbLink href="#">Breadcrumb</BreadcrumbLink>
+      </BreadcrumbItem>
+    </Breadcrumb>
+  );
+};
+
+export default memo(BreadCrumb);
