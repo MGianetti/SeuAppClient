f6f6fcc89b7135681b1c33244d4c3e5234ba7642
MGianetti <mateusgianetti1996@gmail.com>
2022-08-08 21:51:45 -0300

    feat(pages/professionals): Implemented professionals

--- src/pages/professionals/professionals.component.jsx
+++ src/pages/professionals/professionals.component.jsx
@@ -1,7 +1,139 @@
-import React, { memo } from 'react';
+import React, { memo, useState } from 'react';
 
-function Professionals() {
-  return 'Professionals page';
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+  Heading,
+  Input,
+} from '@chakra-ui/react';
+import {
+  PROFESSIONAL_ATTRIBUTES_LABEL,
+  PROFESSIONAL_ERROR,
+  PROFESSIONAL_PAGE_DESCRIPTION,
+  PROFESSIONAL_PAGE_TITLE,
+  PROFESSIONAL_TIPS_LABEL,
+} from './professionals.constants';
+import { CREATE_BUTTON_LABEL } from '../pages.constants';
+
+function Client() {
+  const [name, setName] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [cpf, setCpf] = useState('');
+  const [cnpj, setCnpj] = useState('');
+  const [cellphone, setCellphone] = useState();
+  const [email, setEmail] = useState(30);
+
+  const handleNameChange = e => setName(e.target.value);
+  const handleNicknameChange = e => setNickname(e.target.value);
+  const handleCpfChange = e => setCpf(e.target.value);
+  const handleCnpjChange = e => setCnpj(e.target.value);
+  const handleCellphoneChange = e => setCellphone(e.target.value);
+  const handleEmailChange = e => setEmail(e.target.value);
+
+  // TODO Make error checking handler
+  const isErrorName = false;
+  const isErrorNickname = false;
+  const isErrorCpf = false;
+  const isErrorCnpj = false;
+  const isErrorCellphone = false;
+  const isErrorEmail = false;
+
+  return (
+    <>
+      <Heading w="65%" minW="512px">
+        {PROFESSIONAL_PAGE_TITLE}
+      </Heading>
+      <FormLabel ml="16px" minW="512px" w="65%">
+        {PROFESSIONAL_PAGE_DESCRIPTION}
+      </FormLabel>
+      <Flex flexDir="column" w="65%" minW="512px">
+        <Flex marginTop="32px">
+          <FormControl mr="64px" isRequired isInvalid={isErrorName}>
+            <FormLabel>{PROFESSIONAL_ATTRIBUTES_LABEL.NAME}</FormLabel>
+            <Input type="text" value={name} onChange={handleNameChange} />
+            {!isErrorName ? (
+              <FormHelperText>{PROFESSIONAL_TIPS_LABEL.NAME}</FormHelperText>
+            ) : (
+              <FormErrorMessage>{PROFESSIONAL_ERROR.NAME}</FormErrorMessage>
+            )}
+          </FormControl>
+          <FormControl isRequired isInvalid={isErrorNickname}>
+            <FormLabel>{PROFESSIONAL_ATTRIBUTES_LABEL.NICK_NAME}</FormLabel>
+            <Input
+              type="text"
+              value={nickname}
+              onChange={handleNicknameChange}
+            />
+            {!isErrorNickname ? (
+              <FormHelperText>
+                {PROFESSIONAL_TIPS_LABEL.NICK_NAME}
+              </FormHelperText>
+            ) : (
+              <FormErrorMessage>
+                {PROFESSIONAL_ERROR.NICK_NAME}
+              </FormErrorMessage>
+            )}
+          </FormControl>
+        </Flex>
+        <Flex marginTop="32px">
+          <FormControl mr="64px" isRequired isInvalid={isErrorCpf}>
+            <FormLabel>{PROFESSIONAL_ATTRIBUTES_LABEL.CPF}</FormLabel>
+            <Input type="tel" value={cpf} onChange={handleCpfChange} />
+            {!isErrorCpf ? (
+              <FormHelperText>{PROFESSIONAL_TIPS_LABEL.CPF}</FormHelperText>
+            ) : (
+              <FormErrorMessage>{PROFESSIONAL_ERROR.CPF}</FormErrorMessage>
+            )}
+          </FormControl>
+          <FormControl isRequired isInvalid={isErrorCnpj}>
+            <FormLabel>{PROFESSIONAL_ATTRIBUTES_LABEL.CNPJ}</FormLabel>
+            <Input type="email" value={cnpj} onChange={handleCnpjChange} />
+            {!isErrorCnpj ? (
+              <FormHelperText>{PROFESSIONAL_TIPS_LABEL.CNPJ}</FormHelperText>
+            ) : (
+              <FormErrorMessage>{PROFESSIONAL_ERROR.CNPJ}</FormErrorMessage>
+            )}
+          </FormControl>
+        </Flex>
+        <Flex marginTop="32px">
+          <FormControl mr="64px" isRequired isInvalid={isErrorCellphone}>
+            <FormLabel>{PROFESSIONAL_ATTRIBUTES_LABEL.CELLPHONE}</FormLabel>
+            <Input
+              type="tel"
+              value={cellphone}
+              onChange={handleCellphoneChange}
+            />
+            {!isErrorCellphone ? (
+              <FormHelperText>
+                {PROFESSIONAL_TIPS_LABEL.CELLPHONE}
+              </FormHelperText>
+            ) : (
+              <FormErrorMessage>
+                {PROFESSIONAL_ERROR.CELLPHONE}
+              </FormErrorMessage>
+            )}
+          </FormControl>
+          <FormControl isRequired isInvalid={isErrorEmail}>
+            <FormLabel>{PROFESSIONAL_ATTRIBUTES_LABEL.EMAIL}</FormLabel>
+            <Input type="email" value={email} onChange={handleEmailChange} />
+            {!isErrorEmail ? (
+              <FormHelperText>{PROFESSIONAL_TIPS_LABEL.EMAIL}</FormHelperText>
+            ) : (
+              <FormErrorMessage>{PROFESSIONAL_ERROR.EMAIL}</FormErrorMessage>
+            )}
+          </FormControl>
+        </Flex>
+
+        <Flex w="100%" justifyContent="flex-end" mt="32px">
+          <Button>{CREATE_BUTTON_LABEL}</Button>
+        </Flex>
+      </Flex>
+    </>
+  );
 }
 
-export default memo(Professionals);
+export default memo(Client);
---
+++ src/pages/professionals/professionals.constants.js
@@ -0,0 +1,82 @@
+import {
+  DEFAULT_TIP,
+  DEFAULT_ERROR_MESSAGE_M,
+  INVALID_ERROR_MESSAGE_F,
+} from '../pages.constants';
+
+export const PROFESSIONAL_PAGE_TITLE = 'Novo professional:';
+export const PROFESSIONAL_PAGE_DESCRIPTION = 'Cadastre um novo professional.';
+
+export const SEX_PLACEHOLDER = 'Selecione o sexo';
+
+export const PROFESSIONAL_KEYS = Object.freeze({
+  NAME: 'NAME',
+  NICK_NAME: 'NICK_NAME',
+  CPF: 'CPF',
+  CNPJ: 'CNPJ',
+  CELLPHONE: 'CELLPHONE',
+  EMAIL: 'EMAIL',
+});
+
+export const PROFESSIONAL_ATTRIBUTES_LABEL = Object.freeze({
+  [PROFESSIONAL_KEYS.NAME]: 'Nome',
+  [PROFESSIONAL_KEYS.NICK_NAME]: 'Apelido',
+  [PROFESSIONAL_KEYS.CPF]: 'CPF',
+  [PROFESSIONAL_KEYS.CNPJ]: 'CNPJ',
+  [PROFESSIONAL_KEYS.CELLPHONE]: 'Celular',
+  [PROFESSIONAL_KEYS.EMAIL]: 'Email',
+});
+
+export const PROFESSIONAL_TIPS_LABEL = Object.freeze({
+  [PROFESSIONAL_KEYS.NAME]: DEFAULT_TIP.replace(
+    '{ATTRIBUTE}',
+    PROFESSIONAL_ATTRIBUTES_LABEL.NAME.toLocaleLowerCase()
+  ).replace('{ENTITY}', 'profissional'),
+  [PROFESSIONAL_KEYS.NICK_NAME]: DEFAULT_TIP.replace(
+    '{ATTRIBUTE}',
+    PROFESSIONAL_ATTRIBUTES_LABEL.NICK_NAME.toLocaleLowerCase()
+  ).replace('{ENTITY}', 'profissional'),
+  [PROFESSIONAL_KEYS.CPF]: DEFAULT_TIP.replace(
+    '{ATTRIBUTE}',
+    PROFESSIONAL_ATTRIBUTES_LABEL.CPF.toLocaleLowerCase()
+  ).replace('{ENTITY}', 'profissional'),
+  [PROFESSIONAL_KEYS.CNPJ]: DEFAULT_TIP.replace(
+    '{ATTRIBUTE}',
+    PROFESSIONAL_ATTRIBUTES_LABEL.CNPJ.toLocaleLowerCase()
+  ).replace('{ENTITY}', 'profissional'),
+  [PROFESSIONAL_KEYS.CELLPHONE]: DEFAULT_TIP.replace(
+    '{ATTRIBUTE}',
+    PROFESSIONAL_ATTRIBUTES_LABEL.CELLPHONE.toLocaleLowerCase()
+  ).replace('{ENTITY}', 'profissional'),
+  [PROFESSIONAL_KEYS.EMAIL]: DEFAULT_TIP.replace(
+    '{ATTRIBUTE}',
+    PROFESSIONAL_ATTRIBUTES_LABEL.EMAIL.toLocaleLowerCase()
+  ).replace('{ENTITY}', 'profissional'),
+});
+
+export const PROFESSIONAL_ERROR = Object.freeze({
+  [PROFESSIONAL_KEYS.NAME]: DEFAULT_ERROR_MESSAGE_M.replace(
+    '{ATTRIBUTE}',
+    PROFESSIONAL_ATTRIBUTES_LABEL.NAME.toLocaleLowerCase()
+  ),
+  [PROFESSIONAL_KEYS.NICK_NAME]: DEFAULT_ERROR_MESSAGE_M.replace(
+    '{ATTRIBUTE}',
+    PROFESSIONAL_ATTRIBUTES_LABEL.NICK_NAME.toLocaleLowerCase()
+  ),
+  [PROFESSIONAL_KEYS.CPF]: DEFAULT_ERROR_MESSAGE_M.replace(
+    '{ATTRIBUTE}',
+    PROFESSIONAL_ATTRIBUTES_LABEL.CPF.toLocaleLowerCase()
+  ),
+  [PROFESSIONAL_KEYS.CNPJ]: DEFAULT_ERROR_MESSAGE_M.replace(
+    '{ATTRIBUTE}',
+    PROFESSIONAL_ATTRIBUTES_LABEL.CNPJ.toLocaleLowerCase()
+  ),
+  [PROFESSIONAL_KEYS.CELLPHONE]: INVALID_ERROR_MESSAGE_F.replace(
+    '{ATTRIBUTE}',
+    PROFESSIONAL_ATTRIBUTES_LABEL.CELLPHONE.toLocaleLowerCase()
+  ),
+  [PROFESSIONAL_KEYS.EMAIL]: INVALID_ERROR_MESSAGE_F.replace(
+    '{ATTRIBUTE}',
+    PROFESSIONAL_ATTRIBUTES_LABEL.EMAIL.toLocaleLowerCase()
+  ),
+});
