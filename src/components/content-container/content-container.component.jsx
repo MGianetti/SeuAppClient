778e45d9c28c3460df6a92630c5b9dc8e7d33c57
MGianetti <mateusgianetti1996@gmail.com>
2022-08-08 21:50:12 -0300

    fix(content-container): Fixed height and width responsiveness

--- src/components/content-container/content-container.component.jsx
+++ src/components/content-container/content-container.component.jsx
@@ -19,10 +19,15 @@ const ContentContainer = ({ children }) => {
         />
         <Box
           borderRadius="16px"
-          h="512px"
-          w="95%"
+          padding="64px 32px"
+          h="auto"
+          w="70%"
           bg="white"
+          minW="576px"
           position="relative"
+          display="flex"
+          alignItems="center"
+          flexDirection="column"
           top="-384px"
           border="0px solid rgba(0, 0, 0, 0.125)"
           boxShadow="rgb(0 0 0 / 10%) 0rem 0.25rem 0.375rem -0.0625rem, rgb(0 0 0 / 6%) 0rem 0.125rem 0.25rem -0.0625rem"
