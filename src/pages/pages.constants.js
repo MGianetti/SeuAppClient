d83146fdd780e5d98e6fd2b245c100e74b1c781a
MGianetti <mateusgianetti1996@gmail.com>
2022-08-08 21:52:29 -0300

    feat(app/constants): Implemented global constants

---
+++ src/pages/pages.constants.js
@@ -0,0 +1,6 @@
+export const DEFAULT_TIP = 'Digite aqui o {ATTRIBUTE} do novo {ENTITY}';
+export const CREATE_BUTTON_LABEL = 'Criar';
+export const DEFAULT_ERROR_MESSAGE_M = 'O {ATTRIBUTE} é obrigatório';
+export const DEFAULT_ERROR_MESSAGE_F = 'A {ATTRIBUTE} é obrigatória';
+export const INVALID_ERROR_MESSAGE_F = 'O {ATTRIBUTE} é inválida';
+export const INVALID_ERROR_MESSAGE_M = 'A {ATTRIBUTE} é inválido';
