77a67325383af1c87245acfbde5ebd01f25812cf
MGianetti <mateusgianetti1996@gmail.com>
2022-08-11 19:59:01 -0300

    chore(index.js"s): Refactored exports

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export * from './content-container';
+export * from './logged-in-layout';
+export * from './navbar';
+export * from './side-menu';
+export * from './table';
+export * from './pagination';
+export * from './modal';
---
+++ src/pages/clients/new/index.js
@@ -0,0 +1,1 @@
+export { default } from './new-client.component';
